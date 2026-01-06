dc6a5ea57376d4477dbbda899a6e5014a1e6790a
Ken Everett <the.kds.group@gmail.com>
2026-01-06 11:24:45 -0600

    Update blog.header.js

--- blog.header.js
+++ blog.header.js
@@ -1,6 +1,6 @@
 document.write('<div class="separator" style="clear: both; text-align: left;">');
 document.write('    <figure>'  );
-document.write('      <a target="_blank"href="https://kensbookinfo.blogspot.com/p/support-this-website.html"style="margin-left: 1em; margin-right: 1em;">');
+document.write('      <a target="_blank" href="https://kensbookinfo.blogspot.com/p/support-this-website.html"style="margin-left: 1em; margin-right: 1em;">');
 document.write('        <img border="0" data-original-height="853" data-original-width="1280" height="90" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiOPLW2ZlJs9lPtfgY6Va_LXdodrwaH8rfpdgT9V47cCWRmhDp8Q3T7DTFaGz0VDifyrfak97IiiAmKsohcV_jIfBS4YiendDunTz-pQEmx8FBOAwa8LJmbhVqqKQaK6ISeox0LhRuLjyZr_mLkK-ByEZoG_3FC0QGesXvZKLfIBOYqbyGUywS8bgT0uwk/s320/Contribute.png" width="90" />');
 document.write('        <br />The site is free to use, but it is not without cost. If you find it useful we would appreciate your support by clicking this link and making a contribution to our cause. Thank you!</a>');
 document.write('    </figure>');
