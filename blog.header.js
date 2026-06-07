06bb95abaa872856ad15175052b6feea950d7eb3
Ken Everett <the.kds.group@gmail.com>
2026-06-07 10:27:23 -0500

    Comment out image elements in blog.header.js
    Commented out image elements in the blog header.

--- blog.header.js
+++ blog.header.js
@@ -1,7 +1,7 @@
 document.write('<div class="separator" style="clear: both; text-align: left;">');
 document.write('    <figure>'  );
 document.write('      <a target="_blank" href="https://giving.feedthechildren.org/-/NCCCGCSD?member=SDJMHXBL"style="margin-left: 1em; margin-right: 1em;">');
-// document.write('        <img border="0" data-original-height="853" data-original-width="1280" height="90" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiOPLW2ZlJs9lPtfgY6Va_LXdodrwaH8rfpdgT9V47cCWRmhDp8Q3T7DTFaGz0VDifyrfak97IiiAmKsohcV_jIfBS4YiendDunTz-pQEmx8FBOAwa8LJmbhVqqKQaK6ISeox0LhRuLjyZr_mLkK-ByEZoG_3FC0QGesXvZKLfIBOYqbyGUywS8bgT0uwk/s320/Contribute.png" width="90" />');
+// document.write('        <img border="0" data-original-height="853" data-original-width="1280" height="90" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjvxPidOtpfTG4_ra0zO8KHhjs-6fjpKncmaAwVgdDSjbqq0l9Y3__NV3jZNb-qyq48VMyql-IqKUYbD-lbTbyGi7XSvZNrqlM9N4u82QBFRdrZfanyH2oyktv4byBDlu1GJ_P68D4MHx1ujqqjdb_A6kbI2BEBG3BU7MkaRYNh07KgSy0TVbOFDaPeFmw/s320/feedthechildren_logo_l.webp" width="90" />');
 document.write('        <br />The site is free to use, but it is not without cost. If you find it useful we would appreciate your support by clicking this link and making a contribution to our cause. Thank you!</a>');
 document.write('    </figure>');
 document.write('  </div>');
